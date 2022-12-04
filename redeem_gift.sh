e729edd40d3bbe91e1cb9c60f1ac7b28de7c1043
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-12-04 13:56:46 -0500

    Copy paste and add ./
    Also update the rpc connection time

--- redeem_gift.sh
+++ redeem_gift.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 # TODO auto Generate the monero_wallet: from an open wallet (get_transfers in/out)
 
 # data saved in your 'redeem gift card app / wallet'
+
+# Paste destination address and wallet URI
+pay_to_address="USER INPUT"
+read -p "Destination Address: " pay_to_address
+
+monero_uri="USER INPUT"
+read -p "Paste URI: " monero_uri
+
+# Static node for now
 node="node2.monerodevs.org:38089/json_rpc"
-pay_to_address="59kCsXvXVWg6BBCwXE6Kj5hATsgVGHfSxM5ZJBPrPsPXcxwLza5AdKKTuZhXYrv2dqD1o29mqhzJQJzcBXMuzzGEEuYRaec"
-rpc_binary="monero-wallet-rpc --stagenet"
-# obtained from scanning a qr code
-monero_uri="monero_wallet:address=58Bj65FCpfpULRXyf7mmsY1vB4qiW8qQ8X99tw783rSggPjmvUcRHycaXUQfSwMVpuUj6FWDr4fNHFYyo7f1XdtsJsXAJ1Y&spend_key=75ca1b95ee9dd8bbf059da64c6750fd500731f8775389ce089d516d46108fc05&view_key=c59b3e3182d9c665d0f3a1776a28301410283ab6ff6a9bd3abc7a5bb37758f03&txid=f8477b831a028f07d5638157afc0fbf0897066b4caa29dc48f885fba79cec814,bd20081e0d1abf05b275bcc06bc7e315bc03c57870e247c82c8a45b30f4d1b34,cdbed9b4b2f56de7cce9255610d0cae702aefb36f9a4ff15698ea448f29f6188"
+rpc_binary="./monero-wallet-rpc --stagenet"
 
 #basic sanity checks
 IFS=':' read -ra ADDR <<< "$monero_uri"
-if [[ ${ADDR[0]} != "monero_wallet" ]]; then
-	echo "Not a monero_wallet uri";exit 0
+if [[ ${ADDR[0]} != "monero_wallet" ]];
+	then echo "Not a monero_wallet URI";
+	exit 0
 fi
 
 params_present=0
@@ -65,12 +73,12 @@ $rpc_binary --wallet-dir "$(pwd)" \
 status=""
 while [[ -z "$status" ]]
 do
-	sleep 1
 	printf "\nRPC not available yet..."
+	sleep 3
 	status=$(curl -sk http://localhost:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"stop_wallet"}' -H 'Content-Type: application/json')
 done
 
-echo 
+echo
 rm redeem_gift
 rm redeem_gift.keys
 
